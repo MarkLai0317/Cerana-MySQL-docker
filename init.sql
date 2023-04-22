e58d245fb9c58aabeca5a6554380ce77c26fbcb4
Mark Lai <laimark900317@gmail.com>
2023-04-23 00:14:46 +0800

    init.sql: add new insert statement

--- init.sql
+++ init.sql
@@ -116,3 +116,36 @@ CREATE TABLE `preorder` (
   KEY `user_preorder_idx` (`user_id`),
   CONSTRAINT `user_preorder` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `user` (`user_id`, `user_first_name`, `user_last_name`, `user_email`, `user_how_to_know_us`, `user_phone_number`, `user_create_time`)
+VALUES ('00fkXxesFNbzzXFc5T2GGwQZBOx1', '魏', '可晴', 'waketodo@gmail.com', '學生交流版', '0909503617', '2022-06-17 09:48:18');
+
+INSERT INTO `type` (`type_id`, `type_name`, `user_id`)
+VALUES ('dUoM8j33QOoMufngHglh', '香皂', '00fkXxesFNbzzXFc5T2GGwQZBOx1');
+
+INSERT INTO `product` (`product_id`, `user_id`, `type_id`, `product_name`, `product_price`, `product_spec`, `product_enable`)
+VALUES ('4wbx9iDwlxtxwuxWdxfG', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 'dUoM8j33QOoMufngHglh', '米糠皂', 229, '梔子花', 1),
+       ('M7NCyLbo7bLK8PxqZJp8', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 'dUoM8j33QOoMufngHglh', '米糠皂', 249, '純米皂', 1),
+       ('YhUB8k2KOsGrxn3NbCjr', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 'dUoM8j33QOoMufngHglh', '米糠皂', 229, '白茶樹', 1),
+       ('kszyiuE1OGZkBALDlaDx', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 'dUoM8j33QOoMufngHglh', '米糠皂', 229, '黃香木', 1);
+
+INSERT INTO `discount` (`discount_id`, `user_id`, `discount_value`, `discount_name`, `discount_note`)
+VALUES ('7cIST5sJmrwhfmtHbhHm', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 20, "追蹤九折", 'none');
+
+INSERT INTO `staff` (`staff_id`, `user_id`, `staff_name`, `staff_phone_number`, `staff_email`)
+VALUES ('k7LaWj79lc7kj8Wt6Fx5', '00fkXxesFNbzzXFc5T2GGwQZBOx1', '魏可晴', '0909503617', 'cathie22580@gmail.com');
+
+INSERT INTO `order` (`order_id`, `user_id`, `staff_id`, `order_discount`, `order_note`, `order_create_time`, `order_total_price`)
+VALUES ('GOI83b0EqgCcmEHOh32U', '00fkXxesFNbzzXFc5T2GGwQZBOx1', 'k7LaWj79lc7kj8Wt6Fx5', 0, '110204011', '2022-07-17 09:48:18', 249);
+
+INSERT INTO `order_product` (`order_id`, `product_id`, `op_amount`)
+VALUES ('GOI83b0EqgCcmEHOh32U', 'M7NCyLbo7bLK8PxqZJp8', 1);
+
+INSERT INTO `tag` (`tag_id`, `user_id`, `tag_name`)
+VALUES ('7UhtpM8s1WykcfGIPJkU', '00fkXxesFNbzzXFc5T2GGwQZBOx1', '草莓杯杯');
+
+INSERT INTO `order_tag` (`order_id`, `tage_id`)
+VALUES ('GOI83b0EqgCcmEHOh32U', '7UhtpM8s1WykcfGIPJkU');
+
+INSERT INTO `preorder` (`preorder_id`, `user_id`, `preorder_contact`, `preorder_create_time`, `preorder_is_picked`, `preorder_note`, `preorder_pick_up_time`, `preorder_store_name`)
+VALUES ('3ExP2YKc0PkKY9BWYQs4', '00fkXxesFNbzzXFc5T2GGwQZBOx1', '123', '2022-07-17 09:48:18', 0, 'none', '2022-07-17 09:48:18', 'name');
